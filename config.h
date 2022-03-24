321cca4da0322f6fcbb36fff67cc8ef026770dba
Alex Woersching <ajw006@gmail.com>
2022-03-24 10:55:04 -0700

    Updates personalized config.h
    Adds second (backup) font to config.h. May need to update this font if
    future issues arise.

--- config.h
+++ config.h
@@ -6,6 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "monospace:pixelsize=16";
+static char *font2 = "Roboto Mono for Powerline:pixelsize=16:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
